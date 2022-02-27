10e9d1806067a3ebe27442ba4ae1e8944d8bf034
André Contiero <andre.contiero@yahoo.com.br>
2022-02-27 18:22:12 -0300

    Trying to fix Heroku's error (502)

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,8 @@ async function bootstrap() {
 
   setupSwagger(app);
 
-  await app.listen(process.env.PORT || 3000);
+  const Port = process.env.PORT || 3000;
+
+  await app.listen(Port);
 }
 bootstrap();
